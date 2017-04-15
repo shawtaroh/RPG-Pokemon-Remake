f61c0efadc28f0e770f9d09d3be7368cbd46cd4c
Shawtaroh Granzier-Nakajima <shawtarohg@email.arizona.edu>
2017-04-15 15:39:35 -0700

    Added clouds and transperant menu

--- src/driver/GameGUI.java
+++ src/driver/GameGUI.java
@@ -1,5 +1,6 @@
 package driver;
 
+import java.awt.AlphaComposite;
 import java.awt.Canvas;
 
 /*
@@ -33,6 +34,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.KeyEvent;
@@ -50,6 +52,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -85,11 +88,41 @@ public class GameGUI extends JFrame implements Runnable {
 	private InputHandler inputHandler;
 	private Map world;
 	private BufferedImage currentImage;
+	private BufferedImage msgBox, clouds, fog;
 	private Player player;
 	private jPanel2 painting;
 	private ObjectWaitingForSongToEnd waiter = new ObjectWaitingForSongToEnd();
 
 	public GameGUI(int map) {
+		try {
+			msgBox = ImageIO.read(GameGUI.class.getResource("/art/msgBox.png"));
+			fog = ImageIO.read(GameGUI.class.getResource("/art/clouds2.png"));
+			clouds = ImageIO.read(GameGUI.class.getResource("/art/clouds3.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		BufferedImage tmpImg2 = new BufferedImage(fog.getWidth(), fog.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d2 = (Graphics2D) tmpImg2.getGraphics();
+		g2d2.setComposite(AlphaComposite.SrcOver.derive(0.5f));
+		// set the transparency level in range 0.0f - 1.0f
+		g2d2.drawImage(fog, 0, 0, null);
+		fog = tmpImg2;
+
+		BufferedImage tmpImg3 = new BufferedImage(clouds.getWidth(), clouds.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d3 = (Graphics2D) tmpImg3.getGraphics();
+		g2d3.setComposite(AlphaComposite.SrcOver.derive(0.5f));
+		// set the transparency level in range 0.0f - 1.0f
+		g2d3.drawImage(clouds, 0, 0, null);
+		clouds = tmpImg3;
+
+		BufferedImage tmpImg = new BufferedImage(msgBox.getWidth(), msgBox.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = (Graphics2D) tmpImg.getGraphics();
+		g2d.setComposite(AlphaComposite.SrcOver.derive(0.5f));
+		// set the transparency level in range 0.0f - 1.0f
+		g2d.drawImage(msgBox, 0, 0, null);
+		msgBox = tmpImg;
 		scale = 1;
 		screen = new BitMap(width, height);
 		keys.add(new Key("up"));
@@ -110,6 +143,7 @@ public class GameGUI extends JFrame implements Runnable {
 		menu.setVisible(false);
 		add(menu);
 		painting = new jPanel2();
+		painting.setFocusable(false);
 		add(painting);
 		pack();
 		setResizable(true);
@@ -149,6 +183,11 @@ public class GameGUI extends JFrame implements Runnable {
 			Object[] options = { "Map One", "Map Two" };
 			int map = JOptionPane.showOptionDialog(null, "choose a map", "choose a map",
 					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			Object[] options2 = { "Normal", "Story" };
+			int winCondition = JOptionPane.showOptionDialog(null, "choose a mode", "choose a mode",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			if (winCondition==1)
+				String message="A trecherous fog has appeared. Find the oracle";
 			SplashScreen execute = new SplashScreen("/art/splash/pika loading.gif", "Loading Safari World!");
 			try {
 				Thread.sleep(1000);
@@ -259,9 +298,27 @@ public class GameGUI extends JFrame implements Runnable {
 					int yScroll = (player.getyPosition());
 					world.render(screen, xScroll, yScroll);
 				}
-				//currentImage = screen.getBufferedImage();
-				//painting.repaint();
+				// currentImage = screen.getBufferedImage();
+				// painting.repaint();
 				g.drawImage(screen.getBufferedImage(), 0, 0, width * scale, height * scale, null);
+				g.drawImage(msgBox, this.player.getxPosition() + 64 * 15, this.player.getyPosition() + 64 * 26,
+						width * scale / 3, height * scale / 32, null);
+				g.setFont(new Font("Arial", Font.BOLD, 24));
+				String message = "Hey! Get good at this game!";
+				g.setColor(Color.WHITE);
+				g.drawString("Message Box", this.player.getxPosition() + 64 * 15, this.player.getyPosition() + 64 * 26);
+				g.drawString(message, this.player.getxPosition() + 64 * 15, this.player.getyPosition() + 64 * 26 + 30);
+				g.drawImage(clouds, (((this.getWidth() - (width) * scale)) / 2) - this.player.getxPosition() * scale,
+						((this.getHeight() - (height) * scale) / 2) - this.player.getyPosition() * scale, width * scale,
+						height * scale / 4, null);
+
+				// g.drawImage(msgBox, this.player.getxPosition()+64*11+25,
+				// this.player.getyPosition()+64*26, width * scale/2, height *
+				// scale/4, null);
+				// g.drawImage(fog, (((this.getWidth() - (width) * scale)) / 2)
+				// - this.player.getxPosition() * scale, ((this.getHeight() -
+				// (height) * scale) / 2) - this.player.getyPosition() * scale,
+				// width * scale, height * scale / 4, null);
 				if (Player.isEnterHome()) {
 					g.setColor(Color.WHITE);
 					g.setFont(new Font("Verdana", Font.BOLD, 25));
@@ -400,7 +457,8 @@ public class GameGUI extends JFrame implements Runnable {
 		}
 
 		public void paintComponent(Graphics g) {
-			//g.drawImage(currentImage, 0, 0, width * scale, height * scale, null);
+			// g.drawImage(currentImage, 0, 0, width * scale, height * scale,
+			// null);
 		}
 
 	}

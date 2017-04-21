444519b4061a2cf170e43c0d1f724c7fbda93f48
Shawtaroh Granzier-Nakajima <shawtarohg@email.arizona.edu>
2017-04-20 19:28:53 -0700

    Graphics Updates

--- src/maps/Map.java
+++ src/maps/Map.java
@@ -44,12 +44,20 @@ public class Map {
 	protected static BitMap tile = BitMap.load("/art/floor/tile.png");
 	protected static BitMap[][] house = BitMap.cut("/art/house/houseOne.png", 64, 64, 0, 0);
 	protected static BitMap tree = BitMap.load("/art/wall/tree.png");
+	protected static BitMap water = BitMap.load("/art/wall/water.png");
 	protected static BitMap flowers = BitMap.load("/art/floor/flowersOne.png");
+	protected static BitMap stone = BitMap.load("/art/floor/inside.png");
 	protected static BitMap bolders = BitMap.load("/art/floor/highGround1.png");
+	protected static BitMap bolders2 = BitMap.load("/art/wall/grassbolder.png");
 	private String name;
 	private Random generator = new Random(335);
 	private boolean areFlowers[][] = new boolean[42][46];
 	private boolean areBolders[][] = new boolean[42][46];
+	private boolean areBolders2[][] = new boolean[42][46];
+	private boolean isWater[][] = new boolean[42][46];
+	private boolean isHouse[][] = new boolean[42][46];
+	private boolean isStone[][] = new boolean[42][46];
+
 
 	public Map(int w, int h, Player player, String name) {
 		this.width = w;
@@ -58,7 +66,26 @@ public class Map {
 		this.name = name;
 		randomizeFlowers();
 		randomizeBolders();
+		setWater();
+		setHouse();
 
+	}
+	
+	public void setHouse(){
+		isHouse[11][11]=true;
+		isHouse[12][11]=true;
+		isHouse[13][11]=true;
+		isHouse[11][12]=true;
+		isHouse[12][12]=true;
+		isHouse[13][12]=true;
+		for(int i=7;i<18;i++)
+			for(int j=9;j<15;j++)
+				isStone[i][j]=true;
+		
+	}
+	
+	public void setWater(){
+		isWater[5][5]=true;
 	}
 
 	public void randomizeFlowers() {
@@ -73,14 +100,14 @@ public class Map {
 		for (int i = 0; i < 41; i++)
 			for (int j = 0; j < 45; j++)
 				if (generator.nextDouble() > .97)
-					areBolders[i][j] = true;
+					areBolders2[i][j] = true;
 
 		boolean[][] maze = new model.MazeGenerator().randomMaze();
 
 		for (int i = 22; i < 38; i++)
 			for (int j = 6; j < 38; j++) {
-				areBolders[i][j] = false;
-				areBolders[j][i] = false;
+				areBolders2[i][j] = false;
+				areBolders2[j][i] = false;
 				if (maze[j-6][i-22]){
 					areBolders[j][i] = true;
 					areBolders[i][j] = true;
@@ -120,8 +147,16 @@ public class Map {
 				} else {
 					if (areFlowers[Math.abs(x) % 41][Math.abs(y) % 46])
 						screen.blit(flowers, x * TILE_WIDTH, y * TILE_HEIGHT);
+					if (areBolders2[Math.abs(x) % 41][Math.abs(y) % 46])
+						screen.blit(bolders2, x * TILE_WIDTH, y * TILE_HEIGHT);
 					if (areBolders[Math.abs(x) % 41][Math.abs(y) % 46])
 						screen.blit(bolders, x * TILE_WIDTH, y * TILE_HEIGHT);
+					if (isWater[Math.abs(x) % 41][Math.abs(y) % 46])
+						screen.blit(water, x * TILE_WIDTH, y * TILE_HEIGHT);
+					if(isStone[Math.abs(x) % 41][Math.abs(y) % 46])
+						screen.blit(stone, x * TILE_WIDTH, y * TILE_HEIGHT);					
+					if(isHouse[Math.abs(x) % 41][Math.abs(y) % 46])
+						screen.blit(house[(Math.abs(x)-11)%3][(Math.abs(y)-11)%2], x * TILE_WIDTH, y * TILE_HEIGHT);
 				}
 
 			}
